663accc63cfdcdac8530f9fd9f60179f496d0379
Andrew Fasano <Andrew.Fasano@ll.mit.edu>
2018-05-02 12:03:12 -0400

    Run AFL in qemu-mode and submit results

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-This is a Python 3 example consumer of the Rode0day API
+#### SimpleCRS
+This Python 3 script is an example consumer of the Rode0day API. It will automatically play in Rode0day competitions using AFL in qemu-mode. At the end of each competition, the script will load the next competition and switch to fuzzing those binaries.
 
 # Installation
 1. `git clone https://github.com/AndrewFasano/simple-crs.git`
 2. `cd simple-crs`
-3. Save your API key into `api_key.txt`
+3. Save your API key provied at https://rode0day.mit.edu/profile into `api_key.txt`
 
 
 # Features
 * Get competition status
 * Get competition files
 * Run challenges with sample input
+* Try to find bugs with afl in qemu mode
 * Submit bug-triggering inputs
 * Caching to minimize rate-limited requests
 
-# Not yet implemented:
-* Any code to actually find bugs
+# Planned features
+* Additional fuzzing backends
--- crs.py
+++ crs.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 
-import requests
 import shutil
 import yaml
 import pickle
 import os
 import tarfile
 import logging
+import glob
+import time
+import shlex
+import threading
+import subprocess
 from datetime import datetime
+import requests
 
-API_TOKEN   = open("api_token.txt").read().strip()
-DL_DIR      = "dl"
-COMP_DIR    = "competitions"
-CACHE_DIR   = "cache"
-
-API_BASE    = "https://rode0day.mit.edu/api/{}/".format(API_TOKEN)
+API_TOKEN = open("api_token.txt").read().strip()
+CACHE_DIR = "cache"
+COMP_DIR  = "competitions"
+API_BASE  = "https://rode0day.mit.edu/api/{}/".format(API_TOKEN)
 
 logging.basicConfig(format='%(levelname)s:\t%(message)s')
 logger = logging.getLogger(__name__)
@@ -23,34 +26,35 @@ logger.setLevel(logging.INFO)
 requests_log = logging.getLogger("requests.packages.urllib3")
 requests_log.setLevel(logging.WARNING)
 
-
-for x in [DL_DIR, COMP_DIR, CACHE_DIR]:
+for x in [COMP_DIR, CACHE_DIR]:
     if not os.path.exists(x):
         os.makedirs(x)
 
 def get_status(force_reload=False):
     """
-    Get status from /latest.yaml, save to .latest.yaml locally
+    Get status from /latest.yaml, save to CACHE_DIR/latest.yaml locally
     Only reload when we're past the end date of the local file
     """
 
-    latest_path = os.path.join(DL_DIR, "latest.yaml")
+    latest_path = os.path.join(CACHE_DIR, "latest.yaml")
     if os.path.isfile(latest_path) and not force_reload:
         try:
             data = pickle.load(open(latest_path, "rb"))
-            if datetime.now() < data['end']:
-                logger.debug("Using cached status becaue {} < {}".format(datetime.now(), data['end']))
+            if datetime.utcnow() < data['end']:
+                logger.debug("Using cached status becaue %s < %s", datetime.utcnow(), data['end'])
                 return data
-        except:
+        except pickle.PickleError:
             logger.warning("Cached latest.yaml was corrupted")
+            raise
 
     r = requests.get(API_BASE+"latest.yaml")
     r.raise_for_status()
 
     try:
         data = yaml.load(r.text)
-    except:
-        raise RuntimeException("Could not load status, got message: {}".format(r.text))
+    except pickle.PickleError:
+        logger.error("Could not load status, got message: %s", r.text)
+        return None
 
     with open(latest_path, "wb") as f:
         pickle.dump(data, f)
@@ -63,7 +67,7 @@ def get_competition(status=None):
     if not status:
         status = get_status()
     dl_gz = status["download_link"]
-    dl_path = os.path.join(DL_DIR, (os.path.basename(dl_gz)))
+    dl_path = os.path.join(CACHE_DIR, (os.path.basename(dl_gz)))
     extract_dir = os.path.join(COMP_DIR, str(status["rode0day_id"]))
     info_yaml = os.path.join(extract_dir, "info.yaml")
 
@@ -91,11 +95,11 @@ def parse_info(status=None):
     if not status:
         status = get_status()
     yaml_file = os.path.join(os.path.join(COMP_DIR, str(status["rode0day_id"])), "info.yaml")
-    if not (os.path.isfile(yaml_file)):
+    if not os.path.isfile(yaml_file):
         raise RuntimeError("Missing info.yaml file: {}".format(yaml_file))
 
     info = yaml.load(open(yaml_file))
-    if not (info["rode0day_id"] == status["rode0day_id"]):
+    if info["rode0day_id"] != status["rode0day_id"]:
         raise RuntimeError("Comeptition and latest disagree about the rode0day_id: {} {}".format(info["rode0day_id"], status["rode0day_id"]))
 
     return info
@@ -112,15 +116,16 @@ def test_run(challenge, status=None):
     binary      = os.path.join(local_dir, challenge["binary_path"])
     input_file  = os.path.join(local_dir, challenge["sample_inputs"][0])
     args        = challenge["binary_arguments"].format(input_file=input_file, install_dir=local_dir)
-    command     = "LD_PRELOAD_DIR={library_dir} {binary} {args}".format(library_dir=library_dir, binary=binary, input_file=input_file, args=args)
-    logger.info("Locally running with sample input: {}".format(command))
+    command     = "LD_PRELOAD_DIR={library_dir} {binary} {args}".format(library_dir=library_dir, binary=binary, args=args)
+    logger.info("Locally running with sample input: %s", command)
     os.system(command)
 
 
 def submit_solution(file_path, challenge_id, status=None):
     """
-    Submit a solution 
+    Submit a solution
         Abort if competition has ended
+        Skip if file already submitted
         Save bug_ids in cache and only print when we find new bugs
     """
 
@@ -130,18 +135,28 @@ def submit_solution(file_path, challenge_id, status=None):
     if challenge_id not in status["challenge_ids"]:
         raise ValueError("Can't submit for challenge with id {} since it's not a part of the current competition ({})".format(challenge_id, status["challenge_ids"]))
 
+    cache_pickle = os.path.join(CACHE_DIR, str(challenge_id)+".pickle")
+    if os.path.isfile(cache_pickle):
+        cache = pickle.load(open(cache_pickle, "rb"))
+    else:
+        cache = {}
+
+    if "submitted_files" not in cache.keys():
+        cache["submitted_files"] = []
+
+    if file_path in cache["submitted_files"]:
+        logger.debug("Skipping %s since we already submitted it", file_path)
+        return
+
     with open(file_path, "rb") as f:
         input_file = f.read()
     r = requests.post(API_BASE+"submit", data={"challenge_id": challenge_id}, files={"input": input_file})
     r.raise_for_status()
     result = yaml.load(r.text)
 
+    cache["submitted_files"].append(file_path)
 
-    cache_pickle = os.path.join(CACHE_DIR, str(challenge_id)+".pickle")
-    if os.path.isfile(cache_pickle):
-        cache = pickle.load(open(cache_pickle, "rb"))
-    else:
-        cache = {}
+
 
     new_bugs = []
     firsts = []
@@ -152,38 +167,121 @@ def submit_solution(file_path, challenge_id, status=None):
                 first = True
                 firsts.append(bug)
             if bug not in cache.keys():
-                cache[bug] = {"first": first, "found_at": datetime.now()}
+                cache[bug] = {"first": first, "found_at": datetime.utcnow()} # Storing local timestamps in UTC as well
                 new_bugs.append(bug)
 
         if len(new_bugs):
-            firsts_str = ("(firsts: {})".format(', '.join(map(str,firsts)))) if len(firsts) else ""
-            logger.info("Found new bug(s) for challenge {}: {} {}".format(challenge_id, ', '.join(map(str,new_bugs)), firsts_str))
-            logger.info("Score is now {}".format(result["score"]))
+            firsts_str = ""
+            if len(firsts):
+                firsts_str = "(firsts: {})".format(', '.join([str(first_id) for first_id in firsts]))
 
-            # Update cache
-            with open(cache_pickle, "wb") as f:
-                pickle.dump(cache, f)
+            logger.info("Found new bug(s) for challenge %d: %s %s",challenge_id, ', '.join([str(bug_id) for bug_id in new_bugs]), firsts_str)
+            logger.info("Score is now %d", result["score"])
 
-    elif result["status"]==1:
+    elif result["status"] == 1:
         logger.warning("No crash with input %s", file_path)
     if result["status"] > 1:
         logger.warning("Error: %s", result['status_s'])
+
+    # Update cache
+    with open(cache_pickle, "wb") as f:
+        pickle.dump(cache, f)
 
 
     logger.debug("%d API requests remaining", result["requests_remaining"])
     return result["bug_ids"]
 
-def main():
+
+def _start_afl(challenge):
+    """
+    Launch subprocess running afl-fuzz in qemu mode
+    Translate file input and stdin into the right syntax for AFL
+    """
+    status = get_status()
+    now_ms = int(round(time.time()*1000))
+
+    local_dir   = os.path.join('', *[COMP_DIR, str(status["rode0day_id"]), challenge["install_dir"]])
+    library_dir = os.path.join(local_dir, challenge["library_dir"])
+    binary      = os.path.join(local_dir, challenge["binary_path"])
+    input_dir   = os.path.dirname(os.path.join(local_dir, challenge["sample_inputs"][0])) # Assuming all input files are in the same directory
+    output_dir  = os.path.join('', *[COMP_DIR, str(status["rode0day_id"]), challenge["install_dir"], "outputs_"+str(now_ms)])
+
+    use_stdin   = challenge["binary_arguments"].endswith("< {input_file}") # "< {input_file}" must be at end
+
+    if use_stdin:
+        args    = challenge["binary_arguments"].replace("< {input_file}", "").format(install_dir=local_dir) # Remove input_file redirect entirely
+    else:
+        args    = challenge["binary_arguments"].format(install_dir=local_dir, input_file="@@") # Input file name @@ is replaced by AFL with the fuzzed filename
+
+    bin_command     = "{binary} {args}".format(binary=binary, args=args)
+    fuzz_command = "afl-fuzz -Q -i {input_dir} -o {output_dir} -- {bin_command}".format(input_dir=input_dir, output_dir=output_dir, bin_command=bin_command)
+
+    logger.info("AFL started with command: %s", fuzz_command)
+    my_env = os.environ.copy()
+    with open(os.devnull, 'w') as devnull:
+        my_env["LD_PRELOAD_DIR"] = library_dir
+        subprocess.run(shlex.split(fuzz_command), stdout=devnull, stderr=subprocess.STDOUT, env=my_env)
+
+def _submit_loop(path, challenge_id):
+    while True:
+        for filepath in glob.glob(path):
+            submit_solution(filepath, challenge_id) # Will only submit new filepaths so we can call repeatedly
+        time.sleep(30)
+
+def compete():
+    """
+    Start a fuzzing and submission thread for each binary. Run until the competition ends
+    """
     status = get_status()
     get_competition(status)
     info = parse_info(status)
+    fuzz_threads = []
     for challenge_name, challenge in info['challenges'].items():
-        #print(challenge, challenge_name)
-        test_run(challenge, status)
+        logger.debug("Processing %s", challenge_name)
+        #test_run(challenge, status)
 
-        # TODO: find bugs, submit those inputs and not this file
+        # Start fuzzer thread
+        t = threading.Thread(target=_start_afl, args=(challenge,))
+        t.daemon = True
+        t.start()
+        fuzz_threads.append(t)
 
-        submit_solution("crs.py", challenge["challenge_id"])
-            
+        # Start submission thread watching AFL output
+        output_path = "./competitions/{}/{}/outputs_*/crashes/*".format(info["rode0day_id"], challenge["install_dir"])
+        t2 = threading.Thread(target=_submit_loop, args=(output_path, challenge["challenge_id"],))
+        t2.daemon = True
+        t2.start()
+
+    for thread in fuzz_threads:
+        # Join threads with timeout corresponding to end of competition
+        # The join is blocking so we calculate the delta_t right before the call to join
+        delta_t = status['end']-datetime.utcnow()
+        thread.join(delta_t.seconds)
+
+def main():
+    # While there's a competition happening, try to find solutions until we finish or time runs out
+    # Then move on to the next competition
+    finished = []
+    while True:
+        status=get_status()
+        if not status:
+            logger.error("Could not get status, will retry in 1 minute")
+            time.sleep(60)
+            continue
+
+        if datetime.utcnow() > status['end']:
+            logger.debug("Provied status if for an endex competition, retrying in 1 minute ")
+            time.sleep(60)
+            continue
+
+        if status['rode0day_id'] in finished:
+            logger.info("Finished with competition %d, sleeping until next starts at %s", status['rode0day_id'], str(status['end']))
+            delta_t = status['end']-datetime.utcnow()
+            time.sleep(delta_t.seconds)
+            continue
+
+        compete()
+        finished.append(status['rode0day_id'])
+
 if __name__ == "__main__":
     main()
